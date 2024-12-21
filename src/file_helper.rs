67ab60d0e91ddffc181b668ee6fc71a80d6f2703
kevin <hyunbin7303@gmail.com>
2024-12-20 19:05:56 -0500

    Update unit tests for file_helper and remove unused method

--- src/file_helper.rs
+++ src/file_helper.rs
@@ -1,25 +1,9 @@
-use std::{fs::{self, File},path::PathBuf};
-use std::io::{self, Read};
+use std::{fs::{self},path::PathBuf};
+use std::io::{self};
 pub fn get_files_in_folder(path: &str) -> io::Result<Vec<PathBuf>> {
     let entries = fs::read_dir(path)?;
     let all: Vec<PathBuf> = entries
         .filter_map(|entry| Some(entry.ok()?.path()))
         .collect();
     Ok(all)
-}
-
-fn read_username_from_file(filename: &str) -> Result<String, io::Error> {
-    let username_file_result = File::open(filename);
-
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
-    }
 }
